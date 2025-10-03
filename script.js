d6c63c7c340aec7538a2b8fe641f064f776d5c6a
Origami5822 <sodamm2kp@gmail.com>
2025-10-03 17:37:15 +0700

    new

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const amy_number = document.getElementById("number")
 function add(){
     amy_number.value = Number(amy_number.value) + 1
 }
+function printMessage() {
+    const text = "Test"
+    document.getElementById("message").value = text;
+}
